44b49c4a93a66132f30c052e7199a26796ba60a0
adjiemuliadi <adjiem31@gmail.com>
2026-04-17 15:41:35 +0800

    Initial Commit

---
+++ BetterStocks/BetterStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterStocksApp.swift
+//  BetterStocks
+//
+//  Created by Abuidillah Adjie Muliadi on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
